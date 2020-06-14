ac63772d2372c318c31204ec124decdd2d6b5467
kwin <15951815688@163.com>
2020-06-15 00:18:29 +0800

    整个项目工程

--- gmall-user-service/src/main/java/com/njupt/gmall/user/mapper/UserMapper.java
+++ gmall-user-service/src/main/java/com/njupt/gmall/user/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.njupt.gmall.user.mapper;
 
 import com.njupt.gmall.bean.UmsMember;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
@@ -8,5 +9,7 @@ import tk.mybatis.mapper.common.Mapper;
  * @create 2020-05-11 20:38
  */
 public interface UserMapper extends Mapper<UmsMember> {
+
+    UmsMember judgeUserIsExsitByPhone(@Param("phone") String phone, @Param("username") String username);
 
 }
