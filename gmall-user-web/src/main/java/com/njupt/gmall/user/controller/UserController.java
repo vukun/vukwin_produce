be64d82b1c86bfccd58232adcf4279cb5b0bfbf8
kwin <15951815688@163.com>
2020-06-15 19:00:11 +0800

    整个项目工程

--- gmall-user-web/src/main/java/com/njupt/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/njupt/gmall/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.njupt.gmall.user.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.njupt.gmall.bean.UmsMember;
 import com.njupt.gmall.bean.UmsMemberReceiveAddress;
 import com.njupt.gmall.service.UmsMemberReceiveAddressService;
 import com.njupt.gmall.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -15,7 +19,6 @@ import java.util.List;
  * @create 2020-05-11 20:35
  */
 @Controller
-@RequestMapping("user")
 public class UserController {
 
     @Reference
@@ -24,10 +27,50 @@ public class UserController {
     @Reference
     UmsMemberReceiveAddressService umsMemberReceiveAddressService;
 
-    @RequestMapping("index")
+    @RequestMapping("checkUsername")
     @ResponseBody
-    public String test(){
-        return "Hello GMall!";
+    public String checkUsername(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String usernameResult = userService.checkUsername(username);
+        return usernameResult;
+    }
+
+    @RequestMapping("checkPhone")
+    @ResponseBody
+    public String checkPhone(HttpServletRequest request){
+        String phone = request.getParameter("phone");
+        String phoneResult = userService.checkPhone(phone);
+        return phoneResult;
+    }
+
+
+    @RequestMapping("register")
+    public ModelAndView register( HttpServletRequest request, ModelMap modelMap){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String phone = request.getParameter("phone");
+        UmsMember umsMember = new UmsMember();
+        umsMember.setUsername(username);
+        umsMember.setPassword(password);
+        umsMember.setPhone(phone);
+        userService.register(umsMember);
+        ModelAndView mv = new ModelAndView("redirect:http://localhost:8085/index?ReturnUrl=http://localhost:8083/index");
+        return mv;
+    }
+
+    @RequestMapping("index")
+    public ModelAndView index(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String phone = request.getParameter("phone");
+        ModelAndView mv = null;
+        if(username != null){
+            String url = "redirect:http://localhost:8080/register?username=" + username + "&password=" + password + "&phone=" + phone;
+            mv = new ModelAndView(url);
+        }else{
+            mv = new ModelAndView("index");
+        }
+        return mv;
     }
 
     @RequestMapping("getUmsMemberAddress")
